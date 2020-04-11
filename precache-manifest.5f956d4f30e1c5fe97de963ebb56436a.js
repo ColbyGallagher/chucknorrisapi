b80531b06c80844f27943833546d20b0f3917296
Colby Gallagher <colbyg4@hotmail.com>
2020-04-11 14:07:37 +1000

    Updates

--- precache-manifest.5f956d4f30e1c5fe97de963ebb56436a.js
+++ precache-manifest.5f956d4f30e1c5fe97de963ebb56436a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d058e375617a9c8a49f6ad12319ac463",
+    "revision": "520d57ab930ca638bdf9f3d595f77ae7",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.07548e1c.chunk.css"
   },
   {
-    "revision": "3d293b3732ad8c1f6995",
+    "revision": "3c3abe26f0fdf57786be",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.7204853d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d293b3732ad8c1f6995",
-    "url": "/static/js/main.dde78977.chunk.js"
+    "revision": "3c3abe26f0fdf57786be",
+    "url": "/static/js/main.c3ecc765.chunk.js"
   },
   {
     "revision": "f8d778630d5ad05512b9",
